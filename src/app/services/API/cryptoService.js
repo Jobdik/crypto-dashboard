adaf839e13fc912f3a451a26dfec42edf4a1c57f
Jobdik <139963892+Jobdik@users.noreply.github.com>
2025-05-24 00:56:12 -0400

    Add: getCryptoTopGainers function to fetch top gainers and losers, favorites coins, new listed coins

--- src/app/services/API/cryptoService.js
+++ src/app/services/API/cryptoService.js
@@ -1,5 +1,5 @@
 import Axios from 'axios';
-
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 // Base URL for CoinGecko public API endpoint
 const API_URL = 'https://api.coingecko.com/api/v3';
 
@@ -55,5 +55,127 @@ export const getCryptoInfos = async (symbol) => {
     // Log error and return null
     console.log("Error in getCryptoInfos:",error);
     return null;
+  }
+};
+
+export const getCryptoFavorite = async (uid) => {
+  if(!uid) return null;
+
+  try{
+    const db = getFirestore();
+    const userRef = doc(db, "users", uid.uid);
+    const userSnap = await getDoc(userRef);
+    // Extract the 'favorite' array or return an empty array if none
+    const favorite = userSnap.data()?.favorite || [];
+
+    if(favorite.length > 0){
+      // Limit to the first 10 favorite coins
+      const ids = favorite.slice(0, 10).join(',');
+      // Fetch market data for the selected coins from CoinGecko API
+      const response = await Axios.get(
+        `${API_URL}/coins/markets`,
+        {
+          params: {
+            vs_currency: 'usd',
+            ids: ids, 
+          },
+        }
+      )
+      const data = response.data || [];
+      // Filter and return only the required fields
+      const filteredData = data.map(coin => ({
+        id: coin.id,
+        name: coin.name,
+        symbol: coin.symbol,
+        image: coin.image,
+        current_price: coin.current_price,
+        total_volume: coin.total_volume,
+        price_change_percentage_24h: coin.price_change_percentage_24h,
+        market_cap_change_percentage_24h: coin.market_cap_change_percentage_24h
+      }));
+      return filteredData || [];
+    }
+    else{
+      return null
+    }
+  } catch (error) {
+    console.log("Error in getCryptoFavorite:",error);
+    return null;
+  }
+
+};
+
+export const getCryptoTopGainersLosers = async () => {
+  try {
+    const response = await Axios.get(
+      `${API_URL}/coins/markets`,
+      {
+        params: {
+          vs_currency: 'usd',
+          order:'percent_change_24h_desc',
+          per_page: 100,
+          page:1,
+          price_change_percentage: '24h',
+        },
+      }
+    );
+    const data = response.data || [];
+
+    // Filter and return only the required fields
+    const filteredData = data.map(coin => ({
+      id: coin.id,
+      name: coin.name,
+      symbol: coin.symbol,
+      image: coin.image,
+      total_volume: coin.total_volume,
+      price_change_percentage_24h: coin.price_change_percentage_24h,
+      market_cap_change_percentage_24h: coin.market_cap_change_percentage_24h
+    }));
+    // Sort data by increasing price_change_percentage_24h  
+    filteredData.sort((a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h);
+
+    // Return top 10 gainers and top 10 losers
+    const topGainers = filteredData.slice(0, 10);
+    const topLosers = filteredData.slice(-10);
+    
+
+    return { topGainers, topLosers } || [];
+
+  }
+  catch (error) {
+   console.log("Error in getCryptoTopGainersLosers:",error);
+   return null; 
+  }
+};
+
+
+export const getCryptoNewListings = async () => {
+  try {
+    const response = await Axios.get(
+      `${API_URL}/coins/markets`,
+      {
+        params: {
+          vs_currency: 'usd',
+          order:'market_cap_asc',
+          per_page: 10,
+          page:1,
+        },
+      }
+    );
+    const data = response.data || [];
+    const filteredData = data.map(coin => ({
+      id: coin.id,
+      name: coin.name,
+      symbol: coin.symbol,
+      image: coin.image,
+      total_volume: coin.total_volume,
+      price_change_percentage_24h: coin.price_change_percentage_24h,
+      market_cap_change_percentage_24h: coin.market_cap_change_percentage_24h
+    }))
+    return filteredData || [];
+  }
+  catch (error) {
+   console.log("Error in getCryptoNewListings:",error);
+   return null; 
   }
 };
